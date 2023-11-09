c5c02f190fecc9a0e8f4aee937946533cab5531a
viwinkumarpadala <viwinkumar.p20@iiits.in>
2023-11-09 22:39:49 +0530

    threads undefined error resolved

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { google } = require('googleapis');
 const { OAuth2 } = google.auth;
 const express = require('express');
 require('dotenv').config();
-const app = express();
+const app = express(); 
 
 //Get the details required from .env file
 const client_id = process.env.client_id;
@@ -54,138 +54,144 @@ const checkAndRespondToEmails = async () => {
     console.log(threads);
 
     //Processing each thread in the inbox
-    for (const thread of threads) {
-        const threadId = thread.id;
+    if(threads){
+        for (const thread of threads) {
+            const threadId = thread.id;
 
-        const getThreadRequest = {
-            userId: 'me',
-            id: threadId,
-        };
+            const getThreadRequest = {
+                userId: 'me',
+                id: threadId,
+            };
 
-        const getThreadResponse = await gmail.users.threads.get(getThreadRequest);
-        // console.log(getThreadResponse.data);
-        const messages = getThreadResponse.data.messages;
+            const getThreadResponse = await gmail.users.threads.get(getThreadRequest);
+            // console.log(getThreadResponse.data);
+            const messages = getThreadResponse.data.messages;
 
-        //if the number of messages>0 we will be processing the data further
-        if (messages.length > 0) {
-            console.log('Entered this block')
-            console.log(messages);
+            //if the number of messages>0 we will be processing the data further
+            if (messages.length > 0) {
+                console.log('Entered this block')
+                console.log(messages);
 
-            //Get the latest message in the thread, for sending messages to only them which didn't have any prior replies
-            const firstMessage = messages[messages.length-1];
+                //Get the latest message in the thread, for sending messages to only them which didn't have any prior replies
+                const firstMessage = messages[messages.length - 1];
 
-            const payload = firstMessage.payload;
-            const headers = payload.headers;
-            let fromAddress = headers.find((header) => header.name === 'From').value.trim();
-            let toAddress = headers.find((header) => header.name === 'To').value.trim();
-            console.log(fromAddress);
-            console.log(toAddress);
+                const payload = firstMessage.payload;
+                const headers = payload.headers;
+                let fromAddress = headers.find((header) => header.name === 'From').value.trim();
+                let toAddress = headers.find((header) => header.name === 'To').value.trim();
+                console.log(fromAddress);
+                console.log(toAddress);
 
-            //Extract the from address, to address from the payload headers
-            fromAddress = fromAddress.match(/<([^>]+)>/);
+                //Extract the from address, to address from the payload headers
+                fromAddress = fromAddress.match(/<([^>]+)>/);
 
-            // toAddress = toAddress.match(/<([^>]+)>/);
-            console.log(fromAddress);
-            console.log(toAddress); 
-            
+                // toAddress = toAddress.match(/<([^>]+)>/);
+                console.log(fromAddress);
+                console.log(toAddress);
 
-            //If both are not null then we will be sending a reply
-            if(fromAddress!=null &&  toAddress!=null){
-                fromAddress = fromAddress[1];
-                // toAddress = toAddress[1];
 
-                console.log(`From: ${fromAddress}`);
-                console.log(`To: ${toAddress}`);
+                //If both are not null then we will be sending a reply
+                if (fromAddress != null && toAddress != null) {
+                    fromAddress = fromAddress[1];
+                    // toAddress = toAddress[1];
 
-                console.log(firstMessage);
+                    console.log(`From: ${fromAddress}`);
+                    console.log(`To: ${toAddress}`);
 
-                const replyMessage = {
-                    to: fromAddress,
-                    subject: `Re: Automated reply`,
-                    text: `Heyy I am viwin's app, and This is an automated reply. I am on a vacation, so I will catch up with you soon!`,
-                };
+                    console.log(firstMessage);
 
-                // Construct the email message in RFC 5322 format
-                const message = [
-                    `To: ${replyMessage.to}`,
-                    `Subject: ${replyMessage.subject}`,
-                    `Content-Type: text/plain; charset="UTF-8"`,
-                    '',
-                    replyMessage.text,
-                ].join('\r\n');
+                    const replyMessage = {
+                        to: fromAddress,
+                        subject: `Re: Automated reply`,
+                        text: `Heyy I am viwin's app, and This is an automated reply. I am on a vacation, so I will catch up with you soon!`,
+                    };
 
-                //Create a send message request
-                const sendMessageRequest = {
-                    userId: 'me',
-                    resource: {
-                        raw: Buffer.from(message).toString('base64'),
-                    },
-                };
+                    // Construct the email message in RFC 5322 format
+                    const message = [
+                        `To: ${replyMessage.to}`,
+                        `Subject: ${replyMessage.subject}`,
+                        `Content-Type: text/plain; charset="UTF-8"`,
+                        '',
+                        replyMessage.text,
+                    ].join('\r\n');
 
-                //Send message using googleapi
-                await gmail.users.messages.send(sendMessageRequest);
-
-                console.log('email sent!!');
-                console.log(`Thread:${firstMessage}`); 
-
-                // List all labels
-                const listLabelsRequest = {
-                    userId: 'me',
-                };
-
-                const listLabelsResponse = await gmail.users.labels.list(listLabelsRequest);
-
-                // Find the 'viwinsapp' label if it exists, and get the ids of the labels unread,inbox
-                const existingLabel = listLabelsResponse.data.labels.find(label => label.name === 'viwinsapp');
-                const existingLabel2 = listLabelsResponse.data.labels.find(label => label.name === 'UNREAD');
-                const existingLabel3 = listLabelsResponse.data.labels.find(label => label.name === 'INBOX');
-
-                let labelId;
-                if (existingLabel) {
-                    labelId = existingLabel.id;
-                } else {
-                    // If the label doesn't exist, create it
-                    const createLabelRequest = {
+                    //Create a send message request
+                    const sendMessageRequest = {
                         userId: 'me',
                         resource: {
-                            name: 'viwinsapp',
-                            labelListVisibility: 'labelShow',
-                            messageListVisibility: 'show',
+                            raw: Buffer.from(message).toString('base64'),
                         },
                     };
 
-                    const createLabelResponse = await gmail.users.labels.create(createLabelRequest);
+                    //Send message using googleapi
+                    await gmail.users.messages.send(sendMessageRequest);
 
-                    // Get the label ID
-                    labelId = createLabelResponse.data.id;
-                    console.log(labelId);
+                    console.log('email sent!!');
+                    console.log(`Thread:${firstMessage}`);
+
+                    // List all labels
+                    const listLabelsRequest = {
+                        userId: 'me',
+                    };
+
+                    const listLabelsResponse = await gmail.users.labels.list(listLabelsRequest);
+
+                    // Find the 'viwinsapp' label if it exists, and get the ids of the labels unread,inbox
+                    const existingLabel = listLabelsResponse.data.labels.find(label => label.name === 'viwinsapp');
+                    const existingLabel2 = listLabelsResponse.data.labels.find(label => label.name === 'UNREAD');
+                    const existingLabel3 = listLabelsResponse.data.labels.find(label => label.name === 'INBOX');
+
+                    let labelId;
+                    if (existingLabel) {
+                        labelId = existingLabel.id;
+                    } else {
+                        // If the label doesn't exist, create it
+                        const createLabelRequest = {
+                            userId: 'me',
+                            resource: {
+                                name: 'viwinsapp',
+                                labelListVisibility: 'labelShow',
+                                messageListVisibility: 'show',
+                            },
+                        };
+
+                        const createLabelResponse = await gmail.users.labels.create(createLabelRequest);
+
+                        // Get the label ID
+                        labelId = createLabelResponse.data.id;
+                        console.log(labelId);
+                    }
+
+                    // Apply the label to the thread
+                    const addLabelRequest = {
+                        userId: 'me',
+                        id: threadId,
+                        resource: {
+                            addLabelIds: [labelId],
+                            removeLabelIds: [existingLabel2.id, existingLabel3.id],
+                        },
+                    };
+
+                    await gmail.users.threads.modify(addLabelRequest);
+
+
                 }
 
-                // Apply the label to the thread
-                const addLabelRequest = {
-                    userId: 'me',
-                    id: threadId,
-                    resource: {
-                        addLabelIds: [labelId],
-                        removeLabelIds: [existingLabel2.id,existingLabel3.id],
-                    },
-                };
-
-                await gmail.users.threads.modify(addLabelRequest);
 
 
             }
-
-            
-            
         }
     }
+    else{
+    console.log("No emails in your inbox");
+    }
+    
 
 };
 
 
 //Use oAuth and get authenticated using google
-app.get('/auth/google', async (req, res) => {
+app.get('/auth/google', async (req, res) => { 
     const oAuth2Client = new OAuth2({
         clientId: client_id,
         clientSecret: client_secret,
